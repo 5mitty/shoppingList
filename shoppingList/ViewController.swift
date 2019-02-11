0cf4e7dac05c1d5c2b681f75481f9ab11c8c9688
Jacob Smith <jsmith0361@jh-mba158-19.local>
2019-02-11 11:34:19 -0600

    implemented required functions, added Pcell identifier

--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var barTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
@@ -16,7 +16,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        
     }
 
     @IBAction func whenBarButtonPressed(_ sender: UIBarButtonItem) {
@@ -24,5 +25,14 @@ class ViewController: UIViewController {
         
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
