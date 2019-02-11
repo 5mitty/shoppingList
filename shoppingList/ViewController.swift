cc73aa6f5c57d9713dbe45e9e1501eae7d953d93
Jacob Smith <jsmith0361@jh-mba158-19.local>
2019-02-11 11:14:58 -0600

    created outlets/actions to bar button, textfield, tableView

--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var barTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func whenBarButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+    }
+    
 }
 
